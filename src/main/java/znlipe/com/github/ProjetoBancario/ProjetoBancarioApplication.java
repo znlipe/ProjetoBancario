3362673152ffdda1373c992c4ddad81fd98ecfd0
Felipe <felipezanluca@hotmail.com>
2024-02-27 20:44:47 -0300

    first commit

---
+++ src/main/java/znlipe/com/github/ProjetoBancario/ProjetoBancarioApplication.java
@@ -0,0 +1,13 @@
+package znlipe.com.github.ProjetoBancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBancarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/znlipe/com/github/ProjetoBancario/ProjetoBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package znlipe.com.github.ProjetoBancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
